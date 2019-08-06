3651be8dfdca51588061ed7bf4f686ed652a9747
Jason Lin <157414523@qq.com>
2019-08-06 11:35:23 +0800

    no message

--- assessed_lab_1/randomised-mpunsafe.c
+++ assessed_lab_1/randomised-mpunsafe.c
@@ -59,15 +59,15 @@ int main(int argc, char *argv[]) {
   sum1 = 0;
   #pragma omp parallel private(jloops)
   {
-    #pragma omp for
+    #pragma omp for reduction(+:sum1)
     for (jloops = 0; jloops < nloops; jloops++) {
         drandcur1 = drand48(); // random number between 0,1
         drandcur2 = drandcur1 * drandcur1;
         printf("method2 drandcur2=%g\n", drandcur2);
         sum1 += drandcur1 * drandcur1;
     } 
+    result1 = sum1 / nloops;
   }
-  result1 = sum1 / nloops;
 
   printf("method2 result1=%g\n",result1);
 
