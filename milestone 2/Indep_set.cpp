a6bc488ec3e7ad8905ee186816c8a808cb5ebc9a
Jason Lin <157414523@qq.com>
2019-09-23 11:12:55 +0800

    milestone 2

---
+++ milestone 2/Indep_set.cpp
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string> 
+#include <iostream> 
+#include <sstream> 
+#include <iterator> 
+#include <map>
+#include <list> 
+
+using namespace std; 
+
+string read_line(FILE* file){
+    
+    int next = 0;
+    int size = 0;
+
+    map<int, list<int>> graph;
+    list<int> vertices;
+    list<int> indSet;
+    list<int> indSetMax;
+    string result = ""; 
+
+    int count = 0;
+    while (next != EOF) {
+        next = fgetc(file);
+
+        if(count == 0){
+            result += next;
+            stringstream geek(result); 
+            geek >> size;
+            result = "";
+        } else {
+            if(next != 10 && next != 13 && next != 32){
+                if(next == -1){
+                    if(result == ""){
+                        result = "-1";
+                    }
+                    stringstream geek(result);
+                    int vertex = 0; 
+                    geek >> vertex;
+                    vertices.push_back(vertex);
+                    graph.insert(pair<int, list<int>>(count - 1, vertices));
+                    indSet.push_back(count - 1);
+                    result = "";
+                }
+                result += next;
+            } else if(next == 32 || next == '\n') {
+                if(result == ""){
+                    result = "-1";
+                }
+
+                stringstream geek(result); 
+                int vertex = 0; 
+                geek >> vertex;
+                vertices.push_back(vertex);
+                result = "";
+            }
+        }
+        
+        if(next == '\n'){
+            if(count > 0){
+                graph.insert(pair<int, list<int>>(count - 1, vertices));
+                indSet.push_back(count - 1);
+                vertices = list<int>();
+            }
+            count ++;
+        }
+    }
+
+    map<int, list<int>>::iterator itr; 
+    map<int, list<int>>::iterator itr2;
+    list<int> :: iterator it; 
+
+    /* printing map */ 
+    // cout << "\nThe map is : \n"; 
+    // cout << "\tKEY\tELEMENT\n"; 
+    // for (itr = graph.begin(); itr != graph.end(); ++itr) { 
+    //     cout << '\t' << itr->first ;
+    //     list <int> :: iterator it; 
+    //     for(it = itr->second.begin(); it != itr->second.end(); ++it) {
+    //         cout << '\t' << *it; 
+    //     }
+    //     cout << '\n';
+    // } 
+    // cout << endl;
+
+    /* calculate size of MIS */
+    map<int, list<int>> temGraph = graph;
+    int minCover = 0;
+
+    for (itr = temGraph.begin(); itr != temGraph.end(); ++itr) { 
+        for(it = itr->second.begin(); it != itr->second.end(); ++it) {
+            auto key = temGraph.find(*it);
+            if(key->second.size() > 1){
+                key->second.clear();
+                minCover ++;
+            }
+        }
+    }
+    
+    int maxSize = indSet.size() - minCover;
+
+    /* find MIS */
+    list<int> misTemp = indSet;
+    for(int vertex: misTemp){
+        auto key = graph.find(vertex);
+        for (int v: key->second){
+            misTemp.remove(v);
+        }
+    }
+    indSetMax = misTemp;
+
+    while(indSetMax.size() < maxSize && indSet.size() > 1){
+        for(it = indSetMax.begin(); it != indSetMax.end(); ++it) {
+            misTemp = indSet;
+            misTemp.remove(*it);
+
+            for(int vertex: misTemp){
+                auto key = graph.find(vertex);
+                for (int v: key->second){
+                    misTemp.remove(v);
+                }
+            }
+
+            if(indSetMax.size() < misTemp.size()){
+                indSetMax = misTemp;
+                
+            }
+
+            if(indSetMax.size() == maxSize){
+                break;
+            }
+        }
+        
+        indSet.remove(*indSet.begin());
+    }
+
+    for(it = indSetMax.begin(); it != indSetMax.end(); ++it) {
+        result += to_string(*it) + " ";
+    }
+
+    return result;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3){
+        printf("Error: input arguments less then 3\n");
+        return 0;
+    }
+    //open input file
+    FILE* file = fopen(argv[1], "r");
+
+    if (file == NULL) {
+        printf("Opening file failed!\n");
+        return 0;
+    }
+
+    //read input file
+    string input;
+    input = read_line(file);
+    printf("The maximum independent set is: %s\n", input.c_str());
+    
+    //output file
+    FILE* outputFile;
+    outputFile = fopen(argv[2], "w");
+    
+    fflush(stdout);
+    fprintf(outputFile, "The maximum independent set is: %s\n", input.c_str());
+
+    printf("Input: %s, Output: %s\n", argv[1], argv[2]);
+
+    return 0;
+}
---
+++ milestone 2/mip_test.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string> 
+#include <iostream> 
+#include <sstream> 
+#include <iterator> 
+#include <map>
+#include <list> 
+
+using namespace std; 
+
+int main(int argc, char** argv) {
+    if (argc < 3){
+        printf("Error: input arguments less then 3\n");
+        return 0;
+    }
+    
+    return 0;
+}
