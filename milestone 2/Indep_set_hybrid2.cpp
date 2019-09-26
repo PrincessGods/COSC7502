1df1c66bd33265a0ce19a7ede2df59aeea1f3e4f
Jason Lin <157414523@qq.com>
2019-09-26 11:19:44 +0800

    no message

--- milestone 2/Indep_set_hybrid2.cpp
+++ milestone 2/Indep_set_hybrid2.cpp
@@ -220,7 +220,7 @@ void findMaxIndSet(map<int, list<int>> graph, char* input, char* output) {
         int i;
         #pragma omp parallel private(i)
         {
-            #pragma omp for schedule(dynamic) reduction (+: indSetMaxSize)
+            #pragma omp for reduction (+: indSetMaxSize)
             for(i = 0; i < indSet.size(); i++){
                 if(misTemp[i] != -1) {
                     indSetMaxSize++;
@@ -274,7 +274,7 @@ void findMaxIndSet(map<int, list<int>> graph, char* input, char* output) {
             int i;
             #pragma omp parallel private(i)
             {
-                #pragma omp for schedule(dynamic) reduction (+: removeCount)
+                #pragma omp for reduction (+: removeCount)
                 for(i = 0; i < indSet.size(); i++){
                     if(misTemp[i] == -1) {
                         removeCount++;
