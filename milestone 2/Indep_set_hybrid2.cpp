700f1f70719fa00547039a788f62c0f9cac5854b
Jason Lin <157414523@qq.com>
2019-10-08 12:44:13 +1000

    no message

--- milestone 2/Indep_set_hybrid2.cpp
+++ milestone 2/Indep_set_hybrid2.cpp
@@ -220,7 +220,6 @@ void findMaxIndSet(map<int, list<int>> graph, char* input, char* output) {
             int temMax = 0;
                 #pragma omp for
                 for(i = 0; i < indSet.size(); i++){
-                    printf("Hello from thread %d of %d!\n", omp_get_thread_num(), omp_get_num_threads());
                     if(misTemp[i] != -1) {
                         temMax += 1;
                     }
