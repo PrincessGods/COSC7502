f4e43841375e2fe3347461e44e52c91b6a66d8ee
Jason Lin <157414523@qq.com>
2019-10-15 11:04:47 +1000

    no message

--- milestone 2/Indep_set_hybrid2.cpp
+++ milestone 2/Indep_set_hybrid2.cpp
@@ -196,7 +196,7 @@ void findMaxIndSet(map<int, list<int>> graph, char* input, char* output) {
     }
     
     if(myrank == 0){
-        for(int i = 0; i < indSet.size(); i++){
+        for(int i = begin; i < end; i++){
             //cout << "myrank: " << myrank << " - " << i << '\n';
             auto key = graph.find(misTemp[i]);
             if(key != graph.end()){
